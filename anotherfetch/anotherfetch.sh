b4889375b929307f3c8214122d3a4db32ae087f2
nozomi <confinedrose@duck.com>
2024-07-26 19:29:52 +0800

    Update anotherfetch.sh
    Removed temperature entries, which may vary per directory.

--- anotherfetch/anotherfetch.sh
+++ anotherfetch/anotherfetch.sh
@@ -13,7 +13,6 @@ packages=$(dpkg -l | grep -c '^ii')
 cpu=$(grep -m1 'model name' /proc/cpuinfo | cut -d: -f2 | xargs)
 memory=$(free -h --si | awk '/Mem:/ {print $3 "/" $2}')
 disk=$(df -h / | awk '/\// {print $3 "/" $2 " (" $5 " used)"}')
-temp=$(cat /sys/class/thermal/thermal_zone4/temp | awk '{ print ($1 / 1000) "°C" }')
 
 # Display information
 echo -e "${red}OS: ${reset}$os"
@@ -22,6 +21,5 @@ echo -e "${red}Uptime: ${reset}$uptime"
 echo -e "${red}Kernel: ${reset}$kernel"
 echo -e "${red}Desktop: ${reset}$de"
 echo -e "${red}Packages: ${reset}$packages"
-echo -e "${red}Temperature: ${reset}$temp"
 echo -e "${red}Memory: ${reset}$memory"
 echo -e "${red}Disk: ${reset}$disk"
